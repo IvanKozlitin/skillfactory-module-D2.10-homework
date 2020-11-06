6bbecf3475993d558195793d6664385f7688d9cd
KI-Shadow <ivankozlitin2012@gmail.com>
2020-11-06 18:26:03 +0300

    update

--- my_server.py
+++ my_server.py
@@ -1,7 +1,6 @@
 ﻿import os
 import sentry_sdk
-
-from bottle import Bottle, run, error, HTTPResponse, route
+from bottle import run, route, HTTPResponse
 from sentry_sdk.integrations.bottle import BottleIntegration
 
 sentry_sdk.init(
@@ -9,19 +8,22 @@ sentry_sdk.init(
     integrations=[BottleIntegration()]
 )
 
+
 class NotFound(Exception):
     """
     Используется для идентификации ошибки 404, для последующей отправки в sentry.io
     """
     pass
 
-@error(404)
-def error404(error):
-    raise NotFound("Страница не найдена")
+
+# @error(404)
+# def error404(error):
+#     raise NotFound("Страница не найдена")
 
 @route("/")
 def hello_page():
     return HTTPResponse(200, "Привет!")
+
 
 @route('/fail')
 def index_fail():
@@ -31,6 +33,7 @@ def index_fail():
 @route('/success')
 def index_success():
     return HTTPResponse(200, "Запрос успешный")
+
 
 if os.environ.get('SERVER_URL') == 'https://skillfactory-module-d2-10-home.herokuapp.com':
     run(
