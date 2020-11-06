169499e6924021ea2b56f48b688b47b07cffb184
KI-Shadow <ivankozlitin2012@gmail.com>
2020-11-06 12:08:23 +0300

    Created my server

---
+++ my_server.py
@@ -0,0 +1,24 @@
+import sentry_sdk
+
+from bottle import Bottle
+from sentry_sdk.integrations.bottle import BottleIntegration
+
+sentry_sdk.init(
+    dsn="https://0798cf0e952f479480e8b65241658ba8@o472515.ingest.sentry.io/5506736",
+    integrations=[BottleIntegration()]
+)
+
+app = Bottle()
+
+
+@app.route('/fail')
+def index_fail():
+    raise RuntimeError("There is an error!")
+
+
+@app.route('/success')
+def index_success():
+    return "Запрос успешный"
+
+
+app.run(host='localhost', port=8080)
